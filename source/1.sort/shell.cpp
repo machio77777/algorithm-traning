780cde17f3ab4fb3907b43c1f5aa4613ead1d3cb
tana <gogo71889@gmail.com>
2018-12-26 22:13:40 +0900

    shellsort実装

---
+++ source/1.sort/shell.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long cnt;
+vector<int> G;
+
+// [挿入ソート]
+void insertSort(int A[], int N, int g) {
+    
+    // 間隔gを指定した挿入ソート.
+    for (int i = g; i < N; i++) {
+        int tmp = A[i];
+        int j = i - g;
+        while (j >= 0 && A[j] > tmp) {
+            A[j + g] = A[j];
+            j -= g;
+            cnt++;
+        }
+        A[j + g] = tmp;
+    }
+}
+
+// [シェルソート]
+void shellSort(int A[], int N) {
+    // 数列Gを生成.
+    for (int h = 1; ; ) {
+        if (h > N) {
+            break;
+        }
+        G.push_back(h);
+        h = 3*h + 1;
+    }
+    
+    // 逆順にG[i]=gを指定.
+    for (int i = G.size()-1; i >= 0; i--) {
+        insertSort(A, N, G[i]);
+    }
+}
+
+int main(int argc, char** argv) {
+    
+    int N = 13;
+    int A[] = {17, 14, 4, 2, 5, 10, 7, 1, 6, 3, 9, 6, 11};
+    
+    // シェルソート.
+    shellSort(A, N);
+    
+    
+    for (int i = 0; i < N; i++) {
+        cout << A[i];
+        cout << " ";
+    }
+    return 0;
+}
+
+
